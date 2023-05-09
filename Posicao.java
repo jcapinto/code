5f7902082b1228e3814f1421b3d00b8652b25e66
jcapinto <129993090+jcapinto@users.noreply.github.com>
2023-05-09 10:45:19 +0100

    Update and rename Questões.md to Posicao.java

---
+++ Posicao.java
@@ -0,0 +1,5 @@
+class Posicao{
+  //guardar informação da posiçao escolhida 
+  //implementar construtor, getters e setters
+  //não é necessário proteger/validar os valores recebidos pelos setters!
+}
